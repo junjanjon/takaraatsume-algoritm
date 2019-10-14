0902e7ffea7aa7611015678906d2e6adb8c0c230
junki.hiroi <junki.hiroi@drecom.co.jp>
2019-10-14 18:56:17 +0900

    add build script

--- Assets/Scripts/Editor/Build.cs
+++ Assets/Scripts/Editor/Build.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using UnityEditor;
 
-public class Build
+public static class Build
 {
     /// <summary>「コレクション」の全シーン情報を取得する</summary>
     private static string[] GetAllBuildScenePaths()
@@ -24,4 +24,39 @@ public class Build
             BuildOptions.None  //!< ビルドオプション
         );
     }
+
+    public static string IosBuildDirName     = "XcodeProject";
+    public static string AndroidBuildDirName = "AndroidProject";
+
+    /// <summary>ビルド対象の全シーン
+    public static void IosBuild()
+    {
+        // ビルド対象シーンリスト
+        string[] sceneList = GetAllBuildScenePaths();
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.iOS, "jp.co.drecom.beef.ios.source");
+        // 実行
+        var buildReport = BuildPipeline.BuildPlayer(
+            sceneList,                          //!< ビルド対象シーンリスト
+            Path.Combine(Directory.GetCurrentDirectory(), IosBuildDirName), //!< 出力先
+            BuildTarget.iOS,                    //!< ビルド対象プラットフォーム
+            BuildOptions.Development            //!< ビルドオプション
+        );
+    }
+
+    public static void AndroidBuild()
+    {
+        // ビルド対象シーンリスト
+        string[] sceneList = GetAllBuildScenePaths();
+        PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "jp.test.android.beef");
+        EditorUserBuildSettings.androidBuildSystem = AndroidBuildSystem.Gradle;
+        // 実行
+        var buildReport = BuildPipeline.BuildPlayer(
+            sceneList,                          //!< ビルド対象シーンリスト
+            Path.Combine(Directory.GetCurrentDirectory(), AndroidBuildDirName), //!< 出力先
+            BuildTarget.Android,                    //!< ビルド対象プラットフォーム
+            BuildOptions.AcceptExternalModificationsToPlayer |
+            BuildOptions.Development            //!< ビルドオプション
+        );
+    }
+
 }
